031e3c30ae1a7bf1ecd93880a30bbe40b22de2b5
Miwell <miguelinformatica01@gmail.com>
2020-12-29 12:02:35 +0100

    navbar with react router working

---
+++ src/routers/AppRouter.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
+import { Navbar } from '../components/ui/Navbar';
+
+import { Marvel } from '../components/marvel/Marvel';
+import { LoginScreen } from '../components/login/LoginScreen';
+
+export const AppRouter = () => {
+    return (
+        <Router>
+            <div>
+                <Navbar></Navbar>
+
+                <Switch>
+                    <Route exact path="/login" component={LoginScreen} />
+                    <Route exact path="/" component={Marvel} />
+                </Switch>
+            </div>
+        </Router>
+    );
+}
