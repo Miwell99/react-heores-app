23ae5b5b4402c0b2a8447612fa678b5249ba0f2f
Miwell <miguelinformatica01@gmail.com>
2020-12-31 11:26:10 +0100

    feat: added link to hero screen and validated hero

--- src/components/heroes/HeroList.js
+++ src/components/heroes/HeroList.js
@@ -6,10 +6,6 @@ import './heroScreen.css';
 export const HeroList = ({ publisher }) => {
 
     const heroes = getHeroesByPublisher(publisher);
-    const first = heroes[0];
-    let twoData = [];
-    twoData = [...twoData, first, heroes[1]];
-    console.log(twoData);
 
     return (
         <div className="cards-container">
--- src/components/heroes/HeroScreen.js
+++ src/components/heroes/HeroScreen.js
@@ -1,6 +1,17 @@
 import React from 'react';
+import { Redirect, useParams } from 'react-router-dom';
+import { getHeroById } from '../../selectors/getHeroById';
 
 export const HeroScreen = () => {
+
+    const { heroeId } = useParams();     // Custom Hook from react router
+    const hero = getHeroById(heroeId);
+
+    // hero check.
+    if (!hero) {
+        return <Redirect to="/" />
+    }
+
     return (
         <div>
             <h1>HeroScreen</h1>
