efc76b021623b18472de6f7c8f8c3ffee3ff9e3c
Miwell <miguelinformatica01@gmail.com>
2020-12-31 12:11:06 +0100

    feat: heroScreen view data and go back  action

--- src/components/heroes/HeroScreen.js
+++ src/components/heroes/HeroScreen.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Redirect, useParams } from 'react-router-dom';
 import { getHeroById } from '../../selectors/getHeroById';
 
-export const HeroScreen = () => {
+export const HeroScreen = ({history}) => {
 
     const { heroeId } = useParams();     // Custom Hook from react router
     const hero = getHeroById(heroeId);
@@ -12,9 +12,48 @@ export const HeroScreen = () => {
         return <Redirect to="/" />
     }
 
+    const {
+        superhero,
+        publisher,
+        alter_ego,
+        first_appearance,
+        characters,
+    } = hero;
+
+    const handleGoBack = () => {
+        if(history.lenght<2){
+            // Evade logout
+            history.push('/');
+        }else{
+            history.goBack();
+        }
+    }
+
     return (
-        <div>
-            <h1>HeroScreen</h1>
+        <div className="row mt-5">
+            <div className="col-4">
+                <img
+                    src={`../assets/heroes/${heroeId}.jpg`}
+                    alt={superhero}
+                    className="img-thumbnail"
+                />
+            </div>
+            <div className="col-8">
+                <h3 className="text-center">{superhero}</h3>
+                <ul className="list-group list-group-flush">
+                    <li className="list-group-item"><b>Alter ego: </b>{alter_ego}</li>
+                    <li className="list-group-item"><b>Publisher: </b>{publisher}</li>
+                    <li className="list-group-item"><b>First appearance: </b>{first_appearance}</li>
+                </ul>
+                <h5> Characters</h5>
+                <p>{characters}</p>
+
+                <button
+                    className="btn btn-info"
+                    onClick={handleGoBack}>
+                    Go Back
+                     </button>
+            </div>
         </div>
     )
 }
