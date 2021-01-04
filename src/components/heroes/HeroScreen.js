237f93444fdcbbf2e587ffa9b0594f764f47aa31
Miwell <miguelinformatica01@gmail.com>
2021-01-04 12:54:50 +0100

    fix: HeroScreen margins

--- src/components/heroes/HeroScreen.js
+++ src/components/heroes/HeroScreen.js
@@ -36,24 +36,26 @@ export const HeroScreen = ({ history }) => {
                 <img
                     src={`../assets/heroes/${heroeId}.jpg`}
                     alt={superhero}
-                    className="img-thumbnail animate__animated animate__fadeInTopLeft"
+                    className="img-thumbnail animate__animated animate__fadeInLeftBig"
                 />
             </div>
-            <div className="col-8">
+            <div className="col-8 animate__animated animate__fadeIn">
                 <h3 className="text-center">{superhero}</h3>
                 <ul className="list-group list-group-flush">
                     <li className="list-group-item"><b>Alter ego: </b>{alter_ego}</li>
                     <li className="list-group-item"><b>Publisher: </b>{publisher}</li>
                     <li className="list-group-item"><b>First appearance: </b>{first_appearance}</li>
                 </ul>
-                <h5> Characters</h5>
-                <p>{characters}</p>
+                <div className="m-3">
+                    <h5>Characters</h5>
+                    <p>{characters}</p>
 
-                <button
-                    className="btn btn-info"
-                    onClick={handleGoBack}>
-                    Go Back
+                    <button
+                        className="btn btn-info"
+                        onClick={handleGoBack}>
+                        Go Back
                      </button>
+                </div>
             </div>
         </div>
     )
